0b630956c5a9b22e3960cb5b1874cb85f488a043
Rasmus L Christensen <rasmus.lykke.christensen@gmail.com>
2018-02-07 13:36:35 +0100

    index changed

--- js/myScript.js
+++ js/myScript.js
@@ -3,12 +3,26 @@
  **/
 
 // variabler
-var tal = 1;
+var tal = 111;
+
+// booleans
 var erDetRigtigt = false;
-var tekst = "Her er min tekst";
+var erDetRigtigt = true;
+
+// en streng (noget tekst)
+var veritas = 'Julemanden "findes", og jorden er flad.';
+
+// klister (+)
+var lykke = "Mit navn er Rasmus. Rasmus Lykke.";
+
+// datatyper
+var x = 0;
 
 // kommentarer
 // regning 
+var ind = 2345;
+var ud = 9999;
+
 // datoer
 // klistre strenge sammen
 // lister (scratch: data)
@@ -18,6 +32,8 @@ var tekst = "Her er min tekst";
 // objekter
 
 /* se resultatet af din kode i browserens udviklings-værktøj */
-console.log( "Tallet er lige nu: " + tal );
 
+console.log( "sum:" + (ind - ud) * 12 );
+// skriv i browservinduet
+document.write( "<h1>" + veritas + lykke + "</h1>" );
 // næste gang: events.
